e8cc9056179699590ec3006936c9fb99d3037d17
RedoX <git@redox.ws>
2019-06-19 09:29:39 +0200

    [HttpWrapper][Admin] Add 'unique' to bunny settings

--- http-wrapper/admin/server/settings.php
+++ http-wrapper/admin/server/settings.php
@@ -6,6 +6,7 @@ if(is_array($plugins)) {
         asort($plugins);
 }
 
+$unique=!empty($_POST['unique']);
 ?>
 	      <div class="row">
 	      	<div class="span12">
@@ -23,9 +24,11 @@ if(is_array($plugins)) {
 	<?php endforeach; ?>
 </select>
 <input type="text" class="span5" name="r" value="<?php echo !empty($_POST['r']) ? $_POST['r'] : '' ?>"/><br />
+<input type="checkbox" name="unique" <?php echo $unique ? 'checked="checked"' : ''; ?> /> Unique
 <input type="submit" value="<?php echo __tr("Go !") ?>" class="btn btn-primary"/>
 </form>
 <?php
+$unique_v = array();
 if(isset($_POST['r'])) {
 	$url = "bunnies/settingsForBunnies?action=get&key=".trim($_POST['r']);
 	if($_POST['sender'] != '--')
@@ -35,23 +38,40 @@ if(isset($_POST['r'])) {
 	$data = array();
 	foreach($r['api']['list'] as $k => $v)
 	{
-		$d = $v['item'];
-		$data[$d[0]['key']] = $d[1]['value'];
+    $key = $v['item'][0]['key'];
+    $val = $v['item'][1]['value'];
+
+    if(!$unique)
+  		$data[$key] = $val;
+    else
+    {
+      if(!isset($data[$val])) $data[$val] = array();
+      array_push($data[$val],$key);
+    }
 	}
 	asort($data);
 ?>
 <table class="table table-bordered table-striped span10">
-	<tr>
+  <tr>
 		<th><?php echo __tr('Bunny') ?></th>
 		<th><?php echo __tr('Value') ?></th>
 	</tr>
 <?php
 	foreach($data as $bunny => $value)
-	{
+  {
+    if($unique)
+    {
+      $value = implode(',',$value);
+      if(strlen($value) > 80)
+        $value = substr($value,0, 80).'...';
+      $t = $bunny;
+      $bunny = $value;
+      $value = $t;
+    }
 ?>
 	<tr>
-		<td><?php echo $bunny ?></td>
-		<td><?php echo $value ?></td>
+		<td><?php echo $bunny; ?></td>
+		<td><?php echo $value; ?></td>
 	</tr>
 <?php
 	}
