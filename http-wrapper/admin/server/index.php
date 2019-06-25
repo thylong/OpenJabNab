fb972bec7bcdf5ad631bfe1b66d8a985ca262b7f
RedoX <git@redox.ws>
2019-06-25 17:09:23 +0200

    [HttpWrapper] fix links on admin page

--- http-wrapper/admin/server/index.php
+++ http-wrapper/admin/server/index.php
@@ -278,7 +278,7 @@ function updateBTable(p)
 		<td width="20%" <?php echo in_array($l,$Admins) ? 'style="font-weight:bold;"' :''; ?>><?php echo $l; ?></td>
 		<td><?php echo $name; ?></td>
 		<td width="20%"><?php echo in_array($l,$Online) ? "C":"D&eacute;c"; ?>onnect&eacute;</td>
-		<td><a href="server.php?removeA=<?php echo urlencode($l); ?>"><?php echo __tr('Remove') ?></a> &nbsp;<a href="server.php?reloadA=<?php echo urlencode($l); ?>"><?php echo __tr('Reload') ?></a>&nbsp;<a href="/admin/account_expert.php?acc=<?php echo urlencode($l); ?>"><?php echo __tr('Expert') ?></a></td>
+		<td><a href="?removeA=<?php echo urlencode($l); ?>"><?php echo __tr('Remove') ?></a> &nbsp;<a href="?reloadA=<?php echo urlencode($l); ?>"><?php echo __tr('Reload') ?></a>&nbsp;<a href="account_expert.php?acc=<?php echo urlencode($l); ?>"><?php echo __tr('Expert') ?></a></td>
 	</tr>
 <?php } ?>
 </table>
