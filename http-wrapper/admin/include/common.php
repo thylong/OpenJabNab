5f1419c060a2816e4188065ce8023a2b4e6607f1
RedoX <dev@redox.ws>
2020-04-05 14:48:57 +0200

    [HTTPWrapper] Allow translation, again

--- http-wrapper/admin/include/common.php
+++ http-wrapper/admin/include/common.php
@@ -1,8 +1,8 @@
 <?php
 require_once realpath(dirname(__FILE__)).'/../../include/common.php';
 
-if(!isset($_SESSION['token']) || !$Infos['isAdmin'] || 
-   (strpos($_SERVER['DOCUMENT_URI'],'translation') !== false)
+if(empty($_SESSION['token']) || (!$Infos['isAdmin'] &&
+   (strpos($_SERVER['DOCUMENT_URI'],'translation') === false))
   )
     header('Location: /index.php');
 ?>
