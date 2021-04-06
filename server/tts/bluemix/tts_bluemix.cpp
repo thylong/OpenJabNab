e33d54bdc4f24f30b822f689cc2647cc3a988291
RedoX <git@redox.ws>
2021-04-06 19:41:38 +0200

    [Server][TTS] Fix Bluemix, add DE and IT voices, add tests

--- server/tts/bluemix/tts_bluemix.cpp
+++ server/tts/bluemix/tts_bluemix.cpp
@@ -1,5 +1,7 @@
 #include <QDateTime>
 #include <QCryptographicHash>
+#include <QUuid>
+#include <QJsonDocument>
 #include <QMapIterator>
 #include <QUrl>
 #include <QNetworkRequest>
@@ -63,6 +65,11 @@ TTSbluemix::TTSbluemix():TTSInterface("bluemix", "bluemix")
   v.genre = Voice::Woman;
   voiceList.insert(v.name, v);
 
+  v.name = "fr-FR_NicolasV3Voice";
+  v.label = "Nicolas";
+  v.genre = Voice::Man;
+  voiceList.insert(v.name, v);
+
     // EN
   v.language = "en";
 
@@ -105,6 +112,42 @@ TTSbluemix::TTSbluemix():TTSInterface("bluemix", "bluemix")
   v.label = "KateV3";
   v.genre = Voice::Woman;
   voiceList.insert(v.name, v);
+
+    // DE
+  v.language = "de";
+
+  v.name = "de-DE_BirgitVoice";
+  v.label = "Birgit";
+  v.genre = Voice::Woman;
+  voiceList.insert(v.name, v);
+
+  v.name = "de-DE_BirgitV3Voice";
+  v.label = "BirgitV3";
+  v.genre = Voice::Woman;
+  voiceList.insert(v.name, v);
+
+  v.name = "de-DE_DieterVoice";
+  v.label = "Dieter";
+  v.genre = Voice::Man;
+  voiceList.insert(v.name, v);
+
+  v.name = "de-DE_DieterV3Voice";
+  v.label = "DieterV3";
+  v.genre = Voice::Man;
+  voiceList.insert(v.name, v);
+
+    // IT
+  v.language = "it";
+
+  v.name = "it-IT_FrancescaVoice";
+  v.label = "Francesca";
+  v.genre = Voice::Woman;
+  voiceList.insert(v.name, v);
+
+  v.name = "it-IT_FrancescaV3Voice";
+  v.label = "FrancescaV3";
+  v.genre = Voice::Woman;
+  voiceList.insert(v.name, v);
 }
 
 TTSbluemix::~TTSbluemix()
@@ -143,34 +186,86 @@ QString TTSbluemix::CreateNewSound(QString text, QString voice, bool forceOverwr
     return ttsHTTPUrl.arg(voice, fileName).toLatin1();
   }
 
+  //Old
   //https://text-to-speech-demo.ng.bluemix.net/api/v1/synthesize?text=Bonsoir%2C%20il%20est%2020h35&voice=fr-FR_ReneeV3Voice&download=true&accept=audio%2Fmp3
 
+  //New
+  // POST https://www.ibm.com/demos/live/tts-demo/api/tts/store
+  //      Payload: {"ssmlText":"<prosody pitch=\"default\" rate=\"-0%\">Il est 17h50</prosody>","sessionID":"19315e70-514e-4da9-a39b-8fd27cc9bdc8"}
+  // GET  https://www.ibm.com/demos/live/tts-demo/api/tts/newSynthesize?voice=fr-FR_ReneeV3Voice&id=19315e70-514e-4da9-a39b-8fd27cc9bdc8
+
+  // Session
+  auto sUuid = QUuid::createUuidV5(QUuid::createUuid(),fileName.toUtf8()).toByteArray(QUuid::WithoutBraces);
+  // POST text
+  {
+    QNetworkRequest req(QUrl("https://www.ibm.com/demos/live/tts-demo/api/tts/store"));
+    req.setRawHeader("Content-type","application/json; charset=utf-8");
+    QByteArray ContentData;
+    ContentData +=  "{\"ssmlText\":\"<prosody pitch=\\\"default\\\" rate=\\\"-0%\\\">";
+    
+    ContentData += text.toUtf8();
+    ContentData += "</prosody>\",\"sessionID\":\"";
+    ContentData += sUuid;
+    ContentData += "\"}";
+    //LogDebug(QString("ContentData: %1").arg(QString(ContentData)));
+    auto *rep = _http.post(req,ContentData);
+    QObject::connect(rep, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    QObject::connect(rep, qOverload<QNetworkReply::NetworkError>(&QNetworkReply::error), &loop, &QEventLoop::quit);
+    loop.exec();
+    const auto answer = rep->readAll();
+    delete rep;
+    if(answer.size() == 0)
+    {
+      LogError("TTS Bluemix: Step 1/2 failed. Empty answer");
+      return QString();
+    }
+    //LogDebug(QString("TTS Bluemix: Step 1/2. Answer: %1").arg(QString(answer)));
+    const auto& json = QJsonDocument::fromJson(answer).object();
+    if(!json.contains("status") || !json.contains("message"))
+    {
+      LogError("TTS Bluemix: Step 1/2 failed. Invalid answer");
+      return QString();
+    }
+    const auto status  = json["status"].toString();
+    const auto uuid    = json["message"].toString().replace("stored in: ","");
+    if(status != "success" || uuid.length() == 0)
+    {
+      LogError(QString("TTS Bluemix: Step 1/2 failed. Answer status is %1 instead of success").arg(status));
+      return QString();
+    }
+    /*if(uuid != QString(sUuid))
+    {
+      LogDebug(QString("TTS Bluemix: Step 1/2. UUID changed from %1 to %2").arg(QString(sUuid)).arg(uuid));
+      sUuid = uuid.toUtf8();
+    }*/
+    sUuid = uuid.toUtf8();
+  }
+
   // Fetch MP3
-  QNetworkRequest req(QUrl("https://text-to-speech-demo.ng.bluemix.net/api/v1/synthesize?voice="+voice+"&download=true&accept=audio%2Fmp3&text="+QUrl::toPercentEncoding(text)));
-
-  QNetworkReply* rep = _http.get(req);
-  QObject::connect(rep, &QNetworkReply::finished, &loop, &QEventLoop::quit);
-  QObject::connect(rep, qOverload<QNetworkReply::NetworkError>(&QNetworkReply::error), &loop, &QEventLoop::quit);
-  loop.exec();
-
-  const auto& answer = rep->readAll();
-  if(answer.size() == 0)
   {
-    LogError("TTS Bluemix: Empty file =(");
-    delete rep;
-    return QString();
-  }
+    QNetworkRequest req(QUrl("https://www.ibm.com/demos/live/tts-demo/api/tts/newSynthesize?voice="+voice+"&id="+sUuid));
+    auto* rep = _http.get(req);
+    QObject::connect(rep, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+    QObject::connect(rep, qOverload<QNetworkReply::NetworkError>(&QNetworkReply::error), &loop, &QEventLoop::quit);
+    loop.exec();
 
-  QFile file(filePath);
-  if (!file.open(QIODevice::WriteOnly))
-  {
-    LogError("TTS Bluemix: Cannot open sound file for writing");
+    const auto answer = rep->readAll();
     delete rep;
-    return QString();
+    if(answer.size() == 0)
+    {
+      LogError("TTS Bluemix: Step 2/2 failed. Empty file =(");
+      return QString();
+    }
+
+    QFile file(filePath);
+    if (!file.open(QIODevice::WriteOnly))
+    {
+      LogError("TTS Bluemix: Step 2/2 failed. Cannot open sound file for writing");
+      return QString();
+    }
+    file.write(answer);
+    file.close();
   }
-  file.write(answer);
-  file.close();
-  delete rep;
   return ttsHTTPUrl.arg(voice, fileName).toLatin1();
 }
 
